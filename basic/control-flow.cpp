46ab030cbcf1f2cce3bcdea0de55bfe125fcec54
Abhishek Ghosh <admin@thecustomizewindows.com>
2024-05-09 14:26:30 +0530

    Create control-flow.cpp

---
+++ basic/control-flow.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    int num = 10;
+
+    // If-else statement
+    if (num > 0) {
+        std::cout << "Positive" << std::endl;
+    } else {
+        std::cout << "Non-positive" << std::endl;
+    }
+
+    // For loop
+    for (int i = 0; i < 5; ++i) {
+        std::cout << "Iteration " << i + 1 << std::endl;
+    }
+
+    return 0;
+}
