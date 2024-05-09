470730ecf94c96f5017ff0357bfa25cfbdc7e6e3
Abhishek Ghosh <admin@thecustomizewindows.com>
2024-05-09 14:26:00 +0530

    Create variables.cpp

---
+++ basic/variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    // Integer variable
+    int num = 42;
+
+    // Floating-point variable
+    float pi = 3.14;
+
+    // Character variable
+    char letter = 'A';
+
+    // Output variables
+    std::cout << "Integer: " << num << std::endl;
+    std::cout << "Float: " << pi << std::endl;
+    std::cout << "Character: " << letter << std::endl;
+
+    return 0;
+}
